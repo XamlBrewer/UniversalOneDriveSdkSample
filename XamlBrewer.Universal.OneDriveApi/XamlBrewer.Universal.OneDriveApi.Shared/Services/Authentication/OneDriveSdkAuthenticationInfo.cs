3ee64b82fb76451561bd9c92bf7dd0e9071e9375
XamlBrewer <diederik_krols@hotmail.com>
2015-03-28 09:08:51 +0100

    Integrated the OneNote authenticator.

---
+++ XamlBrewer.Universal.OneDriveApi/XamlBrewer.Universal.OneDriveApi.Shared/Services/Authentication/OneDriveSdkAuthenticationInfo.cs
@@ -0,0 +1,48 @@
+﻿namespace XamlBrewer.Universal.OneDriveApi.Services
+{
+    using OneDrive;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class OneDriveSdkAuthenticationInfo : IAuthenticationInfo
+    {
+        private string accessToken;
+
+        public OneDriveSdkAuthenticationInfo(string token)
+        {
+            this.accessToken = token;
+        }
+
+        public string AccessToken
+        {
+            get { return this.accessToken; }
+        }
+
+        public string RefreshToken
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string TokenType
+        {
+            get { return "Bearer"; }
+        }
+
+        public DateTimeOffset TokenExpiration
+        {
+            get { return new DateTimeOffset(DateTime.MaxValue); }
+        }
+
+        public Task<bool> RefreshAccessTokenAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AuthorizationHeaderValue
+        {
+            get { return string.Concat(TokenType, " ", this.accessToken); }
+        }
+    }
+}
---
+++ XamlBrewer.Universal.OneDriveApi/XamlBrewer.Universal.OneDriveApi.Shared/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace XamlBrewer.Universal.OneDriveApi.ViewModels
+{
+    using Mvvm;
+    using OneDrive;
+    using Services;
+    using System.Windows.Input;
+
+    class MainViewModel : BindableBase
+    {
+
+
+        public ICommand InitializeCommand
+        {
+            get { return new RelayCommand(this.Initialize_Executed); }
+        }
+
+        private async void Initialize_Executed()
+        {
+            await MyOneDrive.Login();
+        }
+    }
+}
