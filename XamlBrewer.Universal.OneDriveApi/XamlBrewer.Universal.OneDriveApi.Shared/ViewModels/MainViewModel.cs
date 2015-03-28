dc9c6346f2a65bff8d1b97d051de3823b4575a76
XamlBrewer <diederik_krols@hotmail.com>
2015-03-28 18:19:40 +0100

    Browsing.

--- XamlBrewer.Universal.OneDriveApi/XamlBrewer.Universal.OneDriveApi.Shared/ViewModels/MainViewModel.cs
+++ XamlBrewer.Universal.OneDriveApi/XamlBrewer.Universal.OneDriveApi.Shared/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@
     using Mvvm;
     using OneDrive;
     using Services;
+    using System.Collections.Generic;
     using System.Diagnostics;
     using System.IO;
     using System.Text;
@@ -10,15 +11,49 @@
 
     class MainViewModel : BindableBase
     {
+        private IEnumerable<ODItem> files;
+        private ODItem selectedFile;
+
+        public IEnumerable<ODItem> Files
+        {
+            get { return files; }
+            set { this.SetProperty(ref files, value); }
+        }
+
+        public ODItem SelectedFile
+        {
+            get { return selectedFile; }
+            set { this.SetProperty(ref selectedFile, value); }
+        }
+
         public ICommand InitializeCommand
         {
             get { return new RelayCommand(this.Initialize_Executed); }
+        }
+
+        public ICommand CreateAssetsCommand
+        {
+            get { return new RelayCommand(this.CreateAssets_Executed); }
+        }
+
+        public ICommand BrowseCommand
+        {
+            get { return new RelayCommand(this.Browse_Executed); }
         }
 
         private async void Initialize_Executed()
         {
             // Login
             await MyOneDrive.Login();
+
+            // Connect to root folder
+            await MyOneDrive.SetRootFolderAsCurrent();
+        }
+
+        private async void CreateAssets_Executed()
+        {
+            // Connect to root folder
+            await MyOneDrive.SetRootFolderAsCurrent();
 
             // Create Assets
             await MyOneDrive.CreateChildFolderInCurrentFolder("OneDrive SDK Test");
@@ -32,6 +67,18 @@
                     string.Format("This is the content of sample file {0}", i).AsStream(),
                     ItemUploadOptions.Default);
             }
+        }
+
+        private async void Browse_Executed()
+        {
+            // Connect to root folder
+            await MyOneDrive.SetRootFolderAsCurrent();
+
+            // Connect to working folder (TODO: refactor API)
+            await MyOneDrive.CreateChildFolderInCurrentFolder("OneDrive SDK Test");
+
+            // Get files
+            this.Files = await MyOneDrive.GetFilesFromCurrentFolder();
         }
     }
 }
